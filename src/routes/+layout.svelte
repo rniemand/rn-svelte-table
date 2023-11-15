e80aaf4da89dbfcbef9a9ae972504ea844e97050
Richard Niemand <niemand.richard@gmail.com>
2023-11-14 18:14:02 -0700

    Stubbing development layout - work in progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import 'bootstrap/dist/css/bootstrap.min.css';
+	import DevNavigationBar from '../components/DevNavigationBar.svelte';
 </script>
 
-<slot />
+<DevNavigationBar />
+<div class="col mx-auto p-3">
+	<main>
+		<slot />
+	</main>
+</div>
+<footer class="pt-5 my-5 text-body-secondary border-top">
+	Created by the <a href="https://github.com/rniemand/rn-svelte-table" target="_blank"
+		>Richard Niemand</a
+	>
+	&middot; {new Date().getFullYear()}
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-body-emphasis">RnSvelteTable</h1>
+<p class="fs-5 col-md-8">More to come.</p>
